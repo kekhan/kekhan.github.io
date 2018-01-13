9caacdc2b94defba5182cb283a04b0212470d7a3
Kerin Khan <kekhan@berkeley.edu>
2018-01-13 14:30:17 -0800

    Update spaceAstroids.js

--- web/games/spaceAstroids/spaceAstroids.js
+++ web/games/spaceAstroids/spaceAstroids.js
@@ -6,6 +6,7 @@ spaceShip.src = 'http://www.pngmart.com/files/3/Spaceship-PNG-File.png';
 laser.src = 'https://donaldcarling.files.wordpress.com/2016/03/blast-harrier-laser-1.png';
 rock.src = 'http://www.freepngimg.com/download/alien/7-2-alien-transparent.png';
 console.log(rock);
+var alienHit =0;
 
 
 var canvas = document.getElementById('canvas');
@@ -83,15 +84,27 @@ function Component(img,x,y,width,height,isBullet,isShip,isComet,color,dx,dy){
 		{
 
 			console.log('alien hit');
+			alienHit+=1;
+			console.log(alienHit);
 			this.width= 0;
+			this.x=0;
+			this.y=0;
+			this.dx=0;
+			this.dy=0;
 			this.height = 0;
+			// should display 000000
+			console.log(this.width,this.height,this.x,this.y,this.dx,this.dy);
+
 			
 		}
+		if(alienHit >= starInitial){
+			alert("YOU WIN!!");
+		}
+		
 	}
 	this.update = function () {
 		if(isBullet){
 			
-			//starts out with 10 bullets
 			if(canvas.key && canvas.key === 32)
 			{
 				bullet.y=ship.y-bullet.height;
