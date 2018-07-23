5152a37a1e420432e8f3305c8aa99667420c3476
Kerin Khan <kerink585@gmail.com>
2018-07-23 11:21:43 -0700

    Update keepBallUp.js
    changed game play so that with every 5 points a new ball comes in the game.

--- web/games/ballGame/keepBallUp.js
+++ web/games/ballGame/keepBallUp.js
@@ -7,7 +7,12 @@ var level=0;
 var bgMusic;
 var ballSound;
 var highestScore=0;
-
+const total = 20;
+var host = 'http://localhost:3000';
+fetch(host + "/highscore").then(resp => resp.json()).then(obj => {
+	highestScore = obj.data;
+	console.log(obj)
+})
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
@@ -16,7 +21,7 @@ var ctx= canvas.getContext('2d');
 ctx.font = "30px Arial";
 ctx.fillStyle="red";
 ctx.fillText("Keep the Ball Up",innerWidth/4,40);
-var colors = ['#4AB94E','#5E5EB3','#823D45','#21A68D','#317BC5'];
+var colors = ['#DDFF39','#1BE865','#ECF8FF','#611AE8','#FF0060'];
 
 window.addEventListener('keydown',function(event){
 	canvas.key= event.keyCode;
@@ -28,7 +33,7 @@ window.addEventListener('keyup',function(event){
 
 function sound(src){
 	/* plays music or sound*/
-	this.sound= document.getElementById('music');
+	this.sound=document.getElementById('music');
 	this.sound.src=src;
 	this.sound.setAttribute('preload','auto');
 	this.sound.style.display="none";
@@ -49,23 +54,22 @@ function Rectangle(x,y,color,width,height,type){
 	this.width=width;
 	this.height=height;
 	this.type=type;
-
 	this.drawRectangle=function(){
 		this.fillStyle=this.color;
 		c.fillRect(this.x,this.y,this.width,this.height);
-		
+
 	}
 
 	this.updateRectangle = function(){
 		if(this.x>innerWidth-100 || this.x<0){
 			this.x=-this.x;
-			
+
 		}
 		if(canvas.key&&canvas.key==37){
-			this.x-=15;
+			this.x-=25;
 		}
 		if(canvas.key&&canvas.key==39){
-			this.x+=15;
+			this.x+=25;
 		}
 		this.drawRectangle();
 	}
@@ -86,29 +90,30 @@ function Circle(x,y,radius,dx,dy,isCircle){
 			c.arc(this.x,this.y,this.radius,0,2*Math.PI);
 			c.fillStyle = this.color;
 			c.fill();
-
 		}
-
 	}
 
 	this.collision = function(){
 		var barline = bar.y-this.height;
 
-		if((this.x +this.radius) < bar.x + bar.width + 45 && (this.x + this.radius) > bar.x &&
+		if((this.x + this.radius) < bar.x + bar.width + 45 && (this.x + this.radius) > bar.x &&
                (this.y+this.radius) < bar.y + bar.height && (this.radius + this.y) > bar.y){
 				barLine = this.y+this.radius;
 				this.dy = -(dy * 2);
 				ballSound.play()
-				
+
 				score++;
 				console.log(score);
 			}
 			else if(this.y>bar.y){
 				if(score>=highestScore){
+
+					fetch(host+'/highscore', {method:'POST', body:JSON.stringify({score:score}), headers:{'content-type':'Application/json'}})
 					highestScore=score;
+
 				}
 				console.log("Game Over");
-				alert("GAME OVER");
+				//alert("GAME OVER");
 				score=0;
 				level=0;
 				//clearInterval(this.interval);
@@ -128,55 +133,62 @@ function Circle(x,y,radius,dx,dy,isCircle){
 		this.x+=this.dx;
 		this.y+=this.dy;
 	}
-
-
 }
 
-
+// declare new objects
 var ballx= Math.random()*innerWidth;
 var bally= Math.random()*innerHeight;
-bar = new Rectangle(innerWidth/4,innerHeight-30,'blue',150,20);
+bar = new Rectangle(innerWidth/4,innerHeight-30,'blue',200,50);
 ball = new Circle(ballx,bally,30,4,4,true);
 Scoretext= new Rectangle("30px", "Consolas", "white", 280, 40, "text");
 ballSound= new sound('bounce.mp3');
+const ballArray = [];
+for(let i=0; i<=total; i++){
+	let x= Math.random()*innerWidth;
+	let y= Math.random()*innerHeight;
+	ballArray.push(new Circle(x,y,30,4,4,true));
+	console.log(ballArray[i]);
+}
+function ballLeveltoScreen(ball){
+	for(var i=0; i<ball;i++){
+		ballArray[i].updateCircle();
+		ballArray[i].collision();
+	}
+
+}
 
 function animate(){
 	/*this function repeatedly calls animate function*/
-	
-	
+
+	//console.log(highestScore);
 	requestAnimationFrame(animate);
-	c.clearRect(0,0,innerWidth,innerHeight);
-	ctx.font="30px Arial";
-    ctx.fillStyle="#5E5EB3"
-    ctx.fillText("Score:"+score,10,50);
-    ctx.fillText("Level:"+level,200,50);
-    ctx.fillText("High Score:"+highestScore,400,50);
-
-    if(score>=30){
-    	ctx.fillText("Great Job!!!",innerWidth/2,innerHeight/2);
-    	level=3;
-
-    }
-    else if (score>=20){
-    	ctx.fillText("Almost Leveling up!!",innerWidth/2,innerHeight/2);
-    	level=2
-    }
-    else if(score>=10){
-    	ctx.fillText("WOO HOOO!!",innerWidth/2,innerHeight/2);
-    	level=1;
-    }
-    else{
-    	console.log("oops");
-    }
-	ball.collision();
+	ctx.clearRect(0,0,innerWidth,innerHeight);
 	bar.updateRectangle();
+	ctx.font="30px Arial";
+  ctx.fillStyle="#5E5EB3"
+  ctx.fillText("Score:"+score,10,50);
+  ctx.fillText("Level:"+level,200,50);
+  ctx.fillText("High Score:"+highestScore,400,50);
 
+
+    if(score<5){
+    	level=0;
+			ballLeveltoScreen(0);
+    }
+    else if (score<10){
+    	level=1
+			ballLeveltoScreen(1);
+    }
+    else if(score<15){
+    	level=3;
+			ballLeveltoScreen(2);
+    }
+		else if(score<20){
+			level=4
+			ballLeveltoScreen(3);
+		}
+	ball.collision();
 	ball.updateCircle();
-
-	
-	
-
-
 
 }
 animate();
