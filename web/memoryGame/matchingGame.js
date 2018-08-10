6bb99fefaf48726f86aaa0839d112070918c8c94
Kerin Khan <kerink585@gmail.com>
2018-08-09 19:55:30 -0700

    Update matchingGame.js

--- web/memoryGame/matchingGame.js
+++ web/memoryGame/matchingGame.js
@@ -1,13 +1,14 @@
 var arrColor = [];
-var matchedArr =[];
+var matchedColorArr =[];
 var arrVal = [];
 var userTurn = true;
 var arrRandom = [1,2,3,4,5,6,7,8,9];
 var timer;
+const matchedNumberArr=[];
 
 function randomColor(){
-	// generate a random color to be assigned to element in 
-	// getclicks 
+	// generate a random color to be assigned to element in
+	// getclicks
 	//colorArr1 = ["red","pink","blue","yellow"];
 	//colorArr2 = ["red","pink","blue","yellow"];
 	// var colorIndex = MAth.floor(Math.random()* colorArr.length);
@@ -15,13 +16,25 @@ function randomColor(){
 
 }
 
+function getRepeat(n){
+	//remove repeated elements from arrays and don't allow user to click on matchedColorArr
+	// matched element
+	/*
+	When user clicks on repeated square, square shoud not change
+
+		*/
+	const repeatBtn = matchedNumberArr.includes(n);
+
+	console.log("MATCH",repeatBtn)
+}
 
 function getClick(btn){
 	//randomize(btn.value);
-	
+
+	var element = btn.value;
+	getRepeat(element);
 	if(userTurn){
-		
-		var element = btn.value;
+
 		console.log(element);
 
 		if(element == "1"){
@@ -48,12 +61,15 @@ function getClick(btn){
 		else if (element == "8"){
 			btn.style.backgroundColor = "pink";
 		}
+		else{
+			console.log("error");
+		}
 		arrColor.push(btn.style.backgroundColor);
 		arrVal.push(btn.value);
 		console.log(arrColor);
 
 		if(arrColor.length == 2 ){
-			userTurn =false;
+			userTurn = false;
 			check2loop(arrColor,arrVal);
 			arrColor =[];
 			arrVal = [];
@@ -63,18 +79,23 @@ function getClick(btn){
 }
 
 function check2loop(arr1,arr2){
+	//checks if the the two button match
 	console.log(arr1,arr2);
 	var color1 = arr1[0];
 	var color2 = arr1[1];
 	var test1 =  "button" + arr2[0];
 	var test2 = "button" + arr2[1];
-	 console.log(test1,test2);
-	 if (color1 == color2){
-
-	 	matchedArr.push(color1);
-	 	matchedArr.push(color2);
+	 console.log("hello"+test1+""+test2);
+	 if (color1 == color2 && test1 != test2){
+		 //if there is a match original color of buttons show
+		 console.log("djkjf",matchedNumberArr);
+		 matchedColorArr.push(color1);
+ 		 matchedColorArr.push(color2);
+ 		 matchedNumberArr.push(arr2[0]);
+ 		 matchedNumberArr.push(arr2[1]);
+		console.log("djkjf",matchedNumberArr);
 	 	checkWin();
-	 	console.log("matchedArr", matchedArr.length);
+	 	console.log("matchedColorArr", matchedColorArr);
 	 	element = document.getElementById(test1);
 	 	otherElement = document.getElementById(test2);
 	 	var otherOriColor = otherElement.style.backgroundColor;
@@ -83,12 +104,13 @@ function check2loop(arr1,arr2){
 		otherElement.style.backgroundColor = 'orange'
 	 	myvar=setTimeout(function(){
 	 	    element.style.backgroundColor = originColr;
-	 	    otherElement.style.backgroundColor =otherOriColor; 
-			clearTimeout(myvar);	
-			}, 1000);
-	 	userTurn =true;
+	 	    otherElement.style.backgroundColor =otherOriColor;
+			clearTimeout(myvar);
+		}, 500);
+	 	userTurn=true;
 	 }
 	 else {
+		 //If not match color of board shows
 	 	element1 = document.getElementById(test1);
 	 	element2 = document.getElementById(test2);
 	 	var el1OriginColor = element1.style.backgroundColor;
@@ -98,14 +120,14 @@ function check2loop(arr1,arr2){
 		element2.style.backgroundColor = el2OriginColor;
 	 	myvar=setTimeout(function(){
 	 	    element1.style.backgroundColor = 'green';
-	 	    element2.style.backgroundColor ='green'; 
-			clearTimeout(myvar);	
-			}, 1000);
-	 	
+	 	    element2.style.backgroundColor ='green';
+			clearTimeout(myvar);
+		}, 500);
+
 	 	userTurn = true;
 
 	 }
-	
+
 }
 
 function timer(){
@@ -119,11 +141,9 @@ function timer(){
 
 function checkWin(){
 	console.log("check winner");
-	if(matchedArr.length == 8){
+	if(matchedColorArr.length == 8){
 		clearInterval(timer);
 		document.getElementById('msg').innerHTML = "Great Job!";
-		
+
 	}
 }
-
-
