5256c7a3f22915a2357fa9c9fc33f63d8c637281
Kerin Khan <kerink585@gmail.com>
2019-01-04 12:11:06 -0800

    recreating blog page

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+//database
+const mongoose = require('mongoose');
+
+//routers
+
+app.get('/',(req,res)=>{
+
+})
+//listening to Server
+app.listen(port,()=>{
+  console.log("Server listening on http://localhost:"+ port);
+})
