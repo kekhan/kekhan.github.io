dd4fb7adcad9a3104b8f449199435b67a9ffa5e0
Kerin Khan <kerink585@gmail.com>
2019-07-04 18:13:08 -0700

    created an objects that moves with scroll event.

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const context=canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height=window.innerHeight;
+let CurrentscrollPosition=0;
+let ticking=false;
 
 //create a class for main object
 
@@ -13,23 +15,38 @@ class Object {
     this.x=x;
     this.y=y;
   }
-  //Getter
+  //Getters
   get position_x(){
     return this.calcPositionX();
   }
-  //Method
-  calcPositionX(){
-    return this.x+=1;
+  get scrollPosition(){
+    return this.calcEvent();
   }
+  //Methods
+  calcPositionX(pos){
+    return this.x=pos*2;
+  }
+
   draw(){
     context.fillRect(this.x,this.y,this.width,this.height);
+    context.fillStyle="#03A6A6";
   }
+
 }
-let square = new Object(50,100,100,100);
-function animate(){
-  context.clearRect(0,0,canvas.width,canvas.height);
-  window.requestAnimationFrame(animate);
-  square.draw();
-  square.position_x;
-}
-//animate();
+let square = new Object(canvas.width*0.2,canvas.height*0.2,100,100);
+window.addEventListener('scroll', function(e){
+  CurrentscrollPosition=window.scrollY;
+  if(!ticking){
+
+    window.requestAnimationFrame(function(){
+      context.clearRect(0,0,canvas.width,canvas.height);
+
+      square.draw();
+      square.calcPositionX(CurrentscrollPosition);
+      ticking=false;
+    });
+
+  }
+  ticking=true;
+
+});
